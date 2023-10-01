7f4ed2f1a5d205f6afb20d324affc6f69b6edd2a
YuriiPotanin <potaninu@i.ua>
2023-10-02 00:37:49 +0300

    a

---
+++ js/01-gallery.js
@@ -0,0 +1,48 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const container = document.querySelector(".gallery");
+
+container.insertAdjacentHTML("afterbegin", createMarkup(galleryItems));
+container.addEventListener("click", handlerClick);
+
+function createMarkup(arr) {
+  return arr
+    .map(
+        ({ preview, original, description }) => `
+        <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+        </li> `
+    )
+    .join(" ");
+}
+
+function handlerClick(evt) {
+    if (evt.target === evt.currentTarget) {
+        return;
+    }
+   evt.preventDefault();
+    
+    const curImage = evt.target;
+    const instance = basicLightbox.create(`
+    <div class="modal">
+        <img src="${curImage.dataset.source}">
+    </div>
+`);
+    instance.show();
+    document.addEventListener('keydown', (event) => {
+        if (event.key !== 'Escape') {
+            return;
+        }
+    instance.close();
+    });
+}
+
+
